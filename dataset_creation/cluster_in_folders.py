665975760ac9c537eee656ffb9d2d5d280c0216a
Pedro Garcia Freitas <pedro.freitas@unb.br>
2025-05-01 22:16:29 -0300

    Refactored dataset_creation/cluster_in_folders.py to use CLI.

--- dataset_creation/cluster_in_folders.py
+++ dataset_creation/cluster_in_folders.py
@@ -4,9 +4,41 @@ import scipy
 import numpy as np
 import shutil
 import tqdm
+import argparse
+
+from absl import app
+from absl.flags import argparse_flags
+
+
+def parse_args(argv):
+    parser = argparse_flags.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--clusters_csv",
+        type=str,
+        help="Directory which contains the distance csvs.",
+        default="/home/lucasabm/datasets/rvl-cdip/images/specification.csv",
+    )
+    parser.add_argument(
+        "--images_folder",
+        type=str,
+        help="Path to the rvl-cdip images.",
+        default="/home/lucasabm/datasets/rvl-cdip/images/specification",
+    )
+    parser.add_argument(
+        "--clusters_dest_folder",
+        type=str,
+        help="Output directory with results",
+        default="/home/lucasabm/datasets/rvl-cdip/clusters/specification",
+    )
+    args = parser.parse_args(argv[1:])
+    return args
+
 
 sh_function = shutil.copy
 # sh_function = shutil.move
+
 
 def move_or_copy(a, b):
     try:
@@ -14,9 +46,6 @@ def move_or_copy(a, b):
     except:
         pass
 
-clusters_csv = "/home/lucasabm/datasets/rvl-cdip/images/specification.csv" # pasta com os csvs
-images_folder = "/home/lucasabm/datasets/rvl-cdip/images/specification" # pasta com as imagens
-clusters_dest_folder = "/home/lucasabm/datasets/rvl-cdip/clusters/specification" # pasta destino
 
 def mkdir(folder):
     try:
@@ -24,40 +53,54 @@ def mkdir(folder):
     except FileExistsError as e:
         print(e)
 
-mkdir(clusters_dest_folder)
 
-df = pd.read_csv(clusters_csv)
-df_clusters = df["cluster_index"].unique()
-cluster_rank = {"cluster": [], "mean_distance": [], "cluster_size": []}
+def main(args):
+    mkdir(args.clusters_dest_folder)
 
-for cluster in df_clusters:
-    cluster_elements = df[df["cluster_index"] == cluster]
-    arrays = [np.fromstring(elem[1:-1], sep=" ") for elem in list(cluster_elements["features"].values)]
-    md = np.mean(scipy.spatial.distance.cdist(arrays, arrays))
-    cluster_rank["mean_distance"].append(md)
-    cluster_rank['cluster'].append(cluster)
-    cluster_rank["cluster_size"].append(len(cluster_elements))
+    df = pd.read_csv(args.clusters_csv)
+    df_clusters = df["cluster_index"].unique()
+    cluster_rank = {"cluster": [], "mean_distance": [], "cluster_size": []}
 
-cluster_rank = pd.DataFrame(cluster_rank)
+    for cluster in df_clusters:
+        cluster_elements = df[df["cluster_index"] == cluster]
+        arrays = [
+            np.fromstring(elem[1:-1], sep=" ")
+            for elem in list(cluster_elements["features"].values)
+        ]
+        md = np.mean(scipy.spatial.distance.cdist(arrays, arrays))
+        cluster_rank["mean_distance"].append(md)
+        cluster_rank["cluster"].append(cluster)
+        cluster_rank["cluster_size"].append(len(cluster_elements))
 
-for group, f in tqdm.tqdm([
-    ("entre 5 e 10", lambda x: (x > 5) & (x <= 10)),
-    ("entre 10 e 20", lambda x: (x > 10) & (x <= 20)),
-    ("mais de 20", lambda x: x > 20),
-    ("5 ou menos", lambda x: x <= 5),
-], 'group'):
-    group_folder = os.path.join(clusters_dest_folder, group)
-    mkdir(group_folder)
+    cluster_rank = pd.DataFrame(cluster_rank)
 
-    group_clusters = cluster_rank[f(cluster_rank["cluster_size"])]
-    ordered_clusters = group_clusters.sort_values(by=["mean_distance"])
+    for group, f in tqdm.tqdm(
+        [
+            ("entre 5 e 10", lambda x: (x > 5) & (x <= 10)),
+            ("entre 10 e 20", lambda x: (x > 10) & (x <= 20)),
+            ("mais de 20", lambda x: x > 20),
+            ("5 ou menos", lambda x: x <= 5),
+        ],
+        "group",
+    ):
+        group_folder = os.path.join(args.clusters_dest_folder, group)
+        mkdir(group_folder)
 
-    for j, row in tqdm.tqdm(enumerate(ordered_clusters.iloc), "cluster"):
-        cluster_index = row["cluster"]
-        cluster_elements = df[df["cluster_index"] == cluster_index]
+        group_clusters = cluster_rank[f(cluster_rank["cluster_size"])]
+        ordered_clusters = group_clusters.sort_values(by=["mean_distance"])
 
-        cluster_folder = os.path.join(group_folder, f"{str(j)} - c{cluster_index}")
-        mkdir(cluster_folder)
+        for j, row in tqdm.tqdm(enumerate(ordered_clusters.iloc), "cluster"):
+            cluster_index = row["cluster"]
+            cluster_elements = df[df["cluster_index"] == cluster_index]
 
-        for file_path in cluster_elements["file_name"].values:
-            move_or_copy(file_path, cluster_folder)
+            cluster_folder = os.path.join(
+                group_folder, f"{str(j)} - c{cluster_index}"
+            )
+            mkdir(cluster_folder)
+
+            for file_path in cluster_elements["file_name"].values:
+                move_or_copy(file_path, cluster_folder)
+
+
+if __name__ == "__main__":
+    app.run(main, flags_parser=parse_args)
