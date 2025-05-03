8e7106daa3eed362bc1e7354ee87c474b9a5681f
Lucas ABM <lucasabmacedo@hotmail.com>
2025-05-02 23:52:45 -0300

    Implements CLI to create-clusters

--- dataset_creation/create_clusters.py
+++ dataset_creation/create_clusters.py
@@ -1,18 +1,41 @@
 from sklearn.cluster import AgglomerativeClustering
 import pandas as pd
-import numpy as np
+import argparse
 
-clusters_csv_path = "/home/lucasabm/datasets/rvl-cdip/images/specification.csv"
-clusters_dest = ""
+from absl import app
+from absl.flags import argparse_flags
 
-agg = AgglomerativeClustering(n_clusters=None, distance_threshold=0.5)
+def parse_args(argv):
+    parser = argparse_flags.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--dists_csv",
+        type=str,
+        help="Directory which contains the distance csvs.",
+        default="~/datasets/rvl-cdip/images/specification.csv",
+    )
+    parser.add_argument(
+        "--clusters_dest_csv",
+        type=str,
+        help="Path for the clusters csv.",
+        default="~/datasets/rvl-cdip/images/specification_clusters.csv",
+    )
+    args = parser.parse_args(argv[1:])
+    return args
 
-df = pd.read_csv(clusters_csv_path)
-df["features"] = df["features"].apply(eval)
+def main(args):
+    agg = AgglomerativeClustering(n_clusters=None, distance_threshold=0.5)
 
-labels = agg.fit_predict(list(df["features"]))
-column_name = "cluster_index"
-if column_name in df.columns:
-    df.drop(column_name, axis=1, inplace=True)
-df.insert(2, "cluster_index", labels)
-df.to_csv(clusters_csv_path, index=False)
+    df = pd.read_csv(args.dists_csv)
+    df["features"] = df["features"].apply(eval)
+
+    labels = agg.fit_predict(list(df["features"]))
+    column_name = "cluster_index"
+    if column_name in df.columns:
+        df.drop(column_name, axis=1, inplace=True)
+    df.insert(2, "cluster_index", labels)
+    df.to_csv(args.clusters_dest_csv, index=False)
+
+if __name__ == "__main__":
+    app.run(main, flags_parser=parse_args)
